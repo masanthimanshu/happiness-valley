4a2bbd7f12001e5639f454f3729b7854579ee7d7
Himanshu Masant <masanthimanshu@gmail.com>
2022-01-13 14:45:00 +0530

    Handlebars JSON Modularization

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import express from "express";
 import { engine } from "express-handlebars";
 import fs from "fs";
 
-const obj = JSON.parse(fs.readFileSync("handlebarsData.json"));
+// Handlebars Objects Starts //
+
+const homeObj = JSON.parse(fs.readFileSync("./handlebarsData/home.json"));
+const aboutObj = JSON.parse(fs.readFileSync("./handlebarsData/about.json"));
+const contactObj = JSON.parse(fs.readFileSync("./handlebarsData/contact.json"));
+const coursesObj = JSON.parse(fs.readFileSync("./handlebarsData/courses.json"));
+const shopObj = JSON.parse(fs.readFileSync("./handlebarsData/shop.json"));
+
+// Handlebars Objects Ends //
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -13,7 +21,23 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req,res) => {
-    res.render("index", obj.homePage);
+    res.render("index", homeObj);
+})
+
+app.get("/about", (req,res)=> {
+  res.render("about", aboutObj);
+})
+
+app.get("/contact", (req,res)=> {
+  res.render("contact", contactObj);
+})
+
+app.get("/courses", (req,res)=> {
+  res.render("courses", coursesObj);
+})
+
+app.get("/shop", (req,res)=> {
+  res.render("shop", shopObj);
 })
 
 app.listen(port, () => {
